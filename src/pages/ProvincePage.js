31a3ad97a2ada943bc7f9de5184b32c4cedf0e34
ethan <ethanzhou762gmail.com>
2021-03-04 18:08:51 -0500

    Deleted appbar cuz it aint finished yet

--- src/pages/ProvincePage.js
+++ src/pages/ProvincePage.js
@@ -1,7 +1,6 @@
 import { Grid } from '@material-ui/core';
 import React from 'react';
 import SimpleMenu from '../components/Menu';
-import AppBar from '../components/Appbar';
 
 const ProvincePage = () => {
   return (
