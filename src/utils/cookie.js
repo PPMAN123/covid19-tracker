fe00fe6ea9363076ccc68f8bb2447454e22ce2d6
ethan <ethanzhou762gmail.com>
2021-03-21 14:32:24 -0400

    Cookie fix

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,11 +1,16 @@
 const checkCookieExistence = (key) =>
   document.cookie.split(';').some((item) => item.trim().startsWith(`${key}=`));
 
-const getCookieValue = (key) =>
-  document.cookie
-    .split('; ')
-    .find((row) => row.startsWith(`${key}=`))
-    .split('=')[1];
+const getCookieValue = (key) => {
+  if (checkCookieExistence(key)) {
+    return document.cookie
+      .split('; ')
+      .find((row) => row.startsWith(`${key}=`))
+      .split('=')[1];
+  } else {
+    return false;
+  }
+};
 
 const setCookieValue = (key, value) => {
   document.cookie = `${key}=${value};`;
